dceb39868a08b96e2f53c2038502617143af1bfd
dublois anthony <dbn3742a@ens.fsi>
2022-09-27 19:31:32 +0200

    classpath

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,24 @@
+package personnages;
+
+public class Gaulois {
+	
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+	
+	public Gaulois(String nom, int force) {
+		super();
+		this.nom = nom;
+		this.force = force;
+	}
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+	
+	
+	
+	
+	
+}
