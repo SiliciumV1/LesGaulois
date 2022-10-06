13e10635cde4416e5acc5a0c29126fd2e100b0dc
SiliciumV1 <anthony@dublois.com>
2022-10-06 14:00:56 +0200

    Update Scenario.java

--- src/histoire/Scenario.java
+++ src/histoire/Scenario.java
@@ -6,7 +6,7 @@ import personnages.Romain;
 public class Scenario {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub test 
 		Gaulois axterix = new Gaulois("Axtérix",10);
 		Romain minus = new Romain("Minus",8);
 		axterix.parler("Bonjour à tous");
