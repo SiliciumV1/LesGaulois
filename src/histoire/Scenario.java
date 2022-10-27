701ee844e66b25aa1d65b6b1dc42247d6fda826c
SiliciumV1 <anthony@dublois.com>
2022-10-27 13:35:11 +0200

    Update Scenario.java

--- src/histoire/Scenario.java
+++ src/histoire/Scenario.java
@@ -12,7 +12,7 @@ public class Scenario {
 		// TODO Auto-generated method stub test
 
 		Druide panoramix = new Druide("panoramix", 5, 9);
-		Gaulois axterix = new Gaulois("Axtérix", 50);
+		Gaulois axterix = new Gaulois("Axtérix", 5);
 		Gaulois Obélix = new Gaulois("Obélix", 8);
 		Romain minus = new Romain("Minus", 15);
 
@@ -21,6 +21,7 @@ public class Scenario {
 		panoramix.preparerPotion();
 		axterix.parler("Bonjour");
 		minus.parler("UN GAU... UN GAUGAU...");
+		panoramix.booster(axterix);
 		axterix.frapper(minus);
 		axterix.frapper(minus);
 		axterix.frapper(minus);
